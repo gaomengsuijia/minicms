7a68e7ee69683db8b4c8003666070f938b6887fa
xiaotu <453105647@qq.com>
2016-08-09 22:29:49 +0800

    家里提交

--- news/admin.py
+++ news/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
  
 from .models import Column, Article
  
- #栏目
+ #栏目后台需要显示的字段
 class ColumnAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'intro','nav_display','home_display')
  
- #文章
+ #文章后台需要显示的字段
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'pub_date', 'update_time')
  
- 
+ #注册模型对象，这样就可以在后台直接显示这个对象，方便管理
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(Article, ArticleAdmin)
